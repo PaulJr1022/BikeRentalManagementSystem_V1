3ae143cb011152f91b0c636d3388e0ab6ff64590
Jesuratnam P. <UT01185@unicomtic.com>
2024-10-11 09:47:48 +0530

    Question2

--- BikeRentalManagementSystem_V1/Bike.cs
+++ BikeRentalManagementSystem_V1/Bike.cs
@@ -13,5 +13,16 @@ namespace BikeRentalManagementSystem_V1
         private string Model { get; set; }
         private decimal RentalPrice { get; set; }
 
+
+        public Bike(string bikeId, string brand, string model, decimal rentalPrice)
+        {
+            BikeId = bikeId;
+            Brand = brand;
+            Model = model;
+            RentalPrice = rentalPrice;
+        }
+
+
+
     }
 }
