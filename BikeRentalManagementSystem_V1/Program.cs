61e14943b27f2e69a3c9048411de2aac6352d9f2
Jesuratnam P. <UT01185@unicomtic.com>
2024-10-11 10:44:20 +0530

    Question5

--- BikeRentalManagementSystem_V1/Program.cs
+++ BikeRentalManagementSystem_V1/Program.cs
@@ -10,6 +10,37 @@ namespace BikeRentalManagementSystem_V1
     {
         static void Main(string[] args)
         {
+            BikeManager manager = new BikeManager();
+            int respones = 0;
+            do
+            {
+                Console.WriteLine("\n Bike Rental Management System" +
+                    "\n 1. Add Bike" +
+                    "\n 2. View All Bikes" +
+                    "\n 3. Update a bike" +
+                    "\n 4. Delete a bike " +
+                    "\n 5. exit ");
+
+                Console.Write("Choose an option");
+
+                switch (respones)
+                {
+                    case 1:
+                        manager.CreateBike();
+                        break;
+                    case 2:
+                        manager.ReadBikes();
+                        break;
+                    case 3:
+                        manager.UpdateBike();
+                        break;
+                    case 4:
+                        manager.DeleteBike();
+                        break;
+
+
+                }
+            } while (respones != 5);
         }
     }
 }
